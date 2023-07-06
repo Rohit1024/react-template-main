c4d26e0394a0316fa8ab34fc3ae3a45326eb24ca
Rohit1024 <55967526+Rohit1024@users.noreply.github.com>
2023-07-06 12:03:23 +0530

    Update firebase.ts

--- react-template-main/src/config/firebase.ts
+++ react-template-main/src/config/firebase.ts
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBGkw7hxyFS13YqoYqWcltb_Dzc33iQqKc",
-    authDomain: "fir-reproduce-19328.firebaseapp.com",
-    databaseURL: "https://fir-reproduce-19328-default-rtdb.firebaseio.com",
-    projectId: "fir-reproduce-19328",
-    storageBucket: "fir-reproduce-19328.appspot.com",
-    messagingSenderId: "372271616914",
-    appId: "1:372271616914:web:53b736b2cbd5f09d0ba74b",
-    measurementId: "G-W3R7WYM6HR"
+
   };
 
 // Initialize Firebase
